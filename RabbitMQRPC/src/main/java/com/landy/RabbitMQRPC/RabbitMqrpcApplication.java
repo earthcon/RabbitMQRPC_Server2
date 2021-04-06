0c8bb45372c14a89a95d37b108b2ee9f47e0ee33
HOME-APT2020\landy <earthcon10@gmail.com>
2021-04-06 21:55:48 +0900

    처음

---
+++ RabbitMQRPC/src/main/java/com/landy/RabbitMQRPC/RabbitMqrpcApplication.java
@@ -0,0 +1,13 @@
+package com.landy.RabbitMQRPC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqrpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitMqrpcApplication.class, args);
+	}
+
+}
---
+++ RabbitMQRPC/src/test/java/com/landy/RabbitMQRPC/RabbitMqrpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.landy.RabbitMQRPC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqrpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
